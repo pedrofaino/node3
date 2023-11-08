815dbd9d42f748b28a875df20e877200628ec77b
pedrofaino <pedrofaino99@gmail.com>
2023-11-08 15:56:33 -0300

    mongoose

--- myApp.js
+++ myApp.js
@@ -1,6 +1,5 @@
 require('dotenv').config();
-var express = require('express')
-var mongoose = require('mongoose')
+let mongoose = require("mongoose");
 
 mongoose.connect(process.env.MONGO_URI,{ useNewUrlParser: true, useUnifiedTopology: true })
 
