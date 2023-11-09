e8dbe9cb9f2891714fabb63fdb85417c7a857c4a
pedrofaino <pedrofaino99@gmail.com>
2023-11-09 14:24:51 -0300

    mongoose

--- myApp.js
+++ myApp.js
@@ -74,10 +74,8 @@ const findPersonById = (personId, done) => {
 
 const findEditThenSave = (personId, done) => {
   const foodToAdd = "hamburger";
-  let person = Person.findById(personId, (err, data) =>{
-    if(err) console.log(err)
-    done(null, data)
-  })
+  let person = Person.findById(personId)
+  console.log(person);
   person.favoriteFoods.push(foodToAdd)
   person.save((err,data)=>{
     if(err) console.log(err)
