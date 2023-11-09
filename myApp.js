9cdbbe0861cd11505e0e63ad667e5673491a4798
pedrofaino <pedrofaino99@gmail.com>
2023-11-09 16:42:57 -0300

    mongoose

--- myApp.js
+++ myApp.js
@@ -96,24 +96,23 @@ const findAndUpdate = (personName, done) => {
 }
 
 const removeById = (personId, done) => {
-  Person.findByIdAndRemove(personId, function(err,data){
-    if(err) {console.log(err)} else done(null,data)
+  Person.findByIdAndRemove(personId, function (err, data) {
+    if (err) { console.log(err) } else done(null, data)
   })
 };
 
 const removeManyPeople = (done) => {
   const nameToRemove = "Mary";
-  Person.remove({name:nameToRemove}, function(err, data){
+  Person.remove({ name: nameToRemove }, function (err, data) {
     console.log(data)
-    if(err) {console.log(err)} else done(null , data);
+    if (err) { console.log(err) } else done(null, data);
   })
 };
 
 const queryChain = (done) => {
   const foodToSearch = "burrito";
-
-  done(null /*, data*/);
-};
+  Person.find({ favoriteFoods: foodToSearch }).sort(name).limit(2).select(!age).exec(done(null, data));
+}
 
 /** **Well Done !!**
 /* You completed these challenges, let's go celebrate !
